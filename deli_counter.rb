5bcf0f9b340396e1a7f2dc8533d2679090f8d0fd
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-19 15:49:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
 katz_deli = []
 
+
 def line(katz_deli)
   the_line = "The line is currently:"
   if katz_deli.size > 0
     katz_deli.each.with_index(1) {|x, index| the_line +=(" #{index}. #{x}")}
     puts the_line
   end
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  end
+    if katz_deli.size == 0
+      puts "The line is currently empty."
+    end
 end 
-    
-    # katz_deli.each.with_index(1) {|x, index| 
+
     
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
