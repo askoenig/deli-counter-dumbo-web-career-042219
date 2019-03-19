20d77d6c5b4678dbdd5a4fb7276790b292a06d94
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-19 15:48:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,8 @@ def now_serving(katz_deli)
   if katz_deli.size > 0
   puts "Now serving #{katz_deli.shift}"
     else
-      puts 
+      puts "There is nobody waiting to be served!"
+    end
+end
   
   
